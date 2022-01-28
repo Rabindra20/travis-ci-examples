29710b8f1e56fef8cb3a16b4acada3319442d0c2
Rabindra <rabindra2079@gmail.com>
2022-01-28 13:09:57 +0545

    travis

--- src/test/java/com/example/travisciexamples/TravisCiExamplesApplicationTests.java
+++ src/test/java/com/example/travisciexamples/TravisCiExamplesApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.travisciexamples;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TravisCiExamplesApplicationTests {
 
 	@Test
 	void contextLoads() {
